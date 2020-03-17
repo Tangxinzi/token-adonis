bb4d9d8e734d2623083f46354949ace15556953e
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2020-03-17 16:13:22 +0800

    调整页面

--- app/Controllers/Http/IndexController.js
+++ app/Controllers/Http/IndexController.js
@@ -1,8 +1,15 @@
 'use strict'
+const superagent    = use('superagent')
+require('superagent-charset')(superagent)
+let result = null
 
 class IndexController {
-  async render ({ request, view }) {
-    return view.render('welcome')
+  async render ({ request, view, params }) {
+    result = await superagent.get('https://travelin-tute.ferer.net/wp-json/wp/v2/paper/' + request.input('id'))
+      .buffer(true)
+      .send()
+
+    return view.render('welcome', {result: result.body})
   }
 }
 
