106cc1228cd53333b4ab7989190755f5f6ad31a7
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2020-04-07 09:58:58 +0800

    完善指定用户进入考场流程

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -8,32 +8,63 @@ class LoginController {
     const data = {
       "id":       request.input('id'),
       "username": request.input('username') || '',
-      "password": request.input('password') || ''
+      "password": request.input('password') || '',
+      "examword": request.input('examword') || ''
     }
 
-    await superagent
-      .post('http://localhost:8888/exam/wp-json/jwt-auth/v1/token')
-      .type('application/json')
-      .send(data)
-      .then(users => {
-        Redis.set('users', JSON.stringify(users.body))
-        response.redirect('paper?id=' + data.id)
-      })
-      .catch(error => {
-        const text = JSON.parse(error.response.text)
+    var paper = await superagent.get('http://localhost:8888/exam/wp-json/wp/v2/paper/' + data.id)
+    paper = JSON.parse(paper.text)
 
-        session.flash({
-          type: 'red',
-          header: text.message,
-          message: JSON.stringify(text.data.status)
+    if (paper.extended_custom_fields.extended_content.options.pass != data.examword) {
+      session.flash({
+        type: 'red',
+        header: '考试密码错误',
+        message: '请重试'
+      })
+
+      response.redirect('back')
+    } else {
+      await superagent
+        .post('http://localhost:8888/exam/wp-json/jwt-auth/v1/token')
+        .type('application/json')
+        .send(data)
+        .then(users => {
+          var point = 0
+          for (var i = 0; i < paper.extended_custom_fields.extended_content.examinee.length; i++) {
+            if (paper.extended_custom_fields.extended_content.examinee[i].user_email == users.body.user_email) {
+              point = 1
+            }
+          }
+
+          if (point) {
+            Redis.set('users', JSON.stringify(users.body))
+            response.redirect('paper?id=' + data.id)
+          } else {
+            session.flash({
+              type: 'red',
+              header: '考生账号未指定',
+              message: '请重试'
+            })
+
+            response.redirect('back')
+          }
         })
+        .catch(error => {
+          const text = JSON.parse(error.response.text)
 
-        response.redirect('back')
-      })
+          session.flash({
+            type: 'red',
+            header: text.message,
+            message: JSON.stringify(text.data.status)
+          })
+
+          response.redirect('back')
+        })
+    }
   }
 
   async render ({ request, view, params }) {
-    var result = await superagent.get('http://localhost:8888/exam/wp-json/wp/v2/paper/' + request.input('id'))
+    const result = await superagent.get('http://localhost:8888/exam/wp-json/wp/v2/paper/' + request.input('id'))
       .buffer(true)
       .send()
 
